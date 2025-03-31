2ad5e20ed6d8a1dd86397f1d5afd1cece8a5c7da
Kassidy <kassidy128@gmail.com>
2025-03-31 01:01:08 +0000

    inference function call fix

--- ml/model.py
+++ ml/model.py
@@ -144,6 +144,6 @@ def performance_on_categorical_slice(
         lb = lb
     )
 
-    preds = inference(model, slice)
+    preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
