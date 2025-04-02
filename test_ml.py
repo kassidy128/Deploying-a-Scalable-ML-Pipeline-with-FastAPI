1cd59bdb5c90d63dcff7936d66a764a9679c923c
Kassidy <kassidy128@gmail.com>
2025-04-02 03:35:54 +0000

    Completed unit tests

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,47 @@
 import pytest
+import random
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from ml.model import train_model
+from train_model import data
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_model_instance():
     """
-    # add description for the first test
+    Tests if the the model used is the RandomForestClassifer
     """
-    # Your code here
-    pass
+    X = np.array([[0, 1, 2], [3, 4, 5]])
+    y = np.array([0, 1])
+
+    model = train_model(X, y)
+    assert isinstance(model, RandomForestClassifier)
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_age_data_range():
     """
-    # add description for the second test
+    Tests the age data against the expected range 
     """
-    # Your code here
-    pass
+    
+    assert data['age'].between(0,100).all()
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_train_test_split():
     """
-    # add description for the third test
+    Tests if the test and train datasets have the expected size
     """
-    # Your code here
-    pass
+    X = np.random.rand(100, 5)
+    y = np.random.randint(0, 2, 100)
+    
+    train_size = 80
+    test_size = 20
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
+
+    assert len(X_train) == train_size, "Training data size does not match expected size"
+    assert len(X_test) == test_size, "Test data size does not match expected size"
+    assert len(y_train) == train_size, "Training labels data size does not match expected size"
+    assert len(y_test) == test_size, "Testing labels data size does not match expected size"
